a3afd7ec45aaa88cdcbbc1017c647f07537942ea
daniilnahl <daniil.nahl@protonmail.com>
2025-07-10 15:10:04 -0700

    Modularized code and removed unnecessary components.

--- sensors/sen54 sdn-t/sen54/sen54.ino
+++ sensors/sen54 sdn-t/sen54/sen54.ino
@@ -57,9 +57,16 @@ void setup() {
     while (!Serial) {
         delay(100);
     }
-
+    
     Wire.begin();
+    SetupSen54();
+}
 
+void loop() {
+    LoopSen54();
+}
+
+void SetupSen54(){
     sen5x.begin(Wire);
 
     uint16_t error;
@@ -71,26 +78,6 @@ void setup() {
         Serial.println(errorMessage);
     }
 
-// Print SEN55 module information if i2c buffers are large enough
-
-    // set a temperature offset in degrees celsius
-    // Note: supported by SEN54 and SEN55 sensors
-    // By default, the temperature and humidity outputs from the sensor
-    // are compensated for the modules self-heating. If the module is
-    // designed into a device, the temperature compensation might need
-    // to be adapted to incorporate the change in thermal coupling and
-    // self-heating of other device components.
-    //
-    // A guide to achieve optimal performance, including references
-    // to mechanical design-in examples can be found in the app note
-    // “SEN5x – Temperature Compensation Instruction” at www.sensirion.com.
-    // Please refer to those application notes for further information
-    // on the advanced compensation settings used
-    // in `setTemperatureOffsetParameters`, `setWarmStartParameter` and
-    // `setRhtAccelerationMode`.
-    //
-    // Adjust tempOffset to account for additional temperature offsets
-    // exceeding the SEN module's self heating.
     float tempOffset = 0.0;
     error = sen5x.setTemperatureOffsetSimple(tempOffset);
     if (error) {
@@ -112,7 +99,7 @@ void setup() {
     }
 }
 
-void loop() {
+void LoopSen54(){ //right now only prints values into serial monitor.
     uint16_t error;
     char errorMessage[256];
 
@@ -130,8 +117,7 @@ void loop() {
 
     error = sen5x.readMeasuredValues(
         massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
-        massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex,
-        noxIndex);
+        massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex, noxIndex);
 
     if (error) {
         Serial.print("Error trying to execute readMeasuredValues(): ");
@@ -170,12 +156,6 @@ void loop() {
         } else {
             Serial.print(vocIndex);
         }
-        Serial.print("\t");
-        Serial.print("NoxIndex:");
-        if (isnan(noxIndex)) {
-            Serial.println("n/a");
-        } else {
-            Serial.println(noxIndex);
-        }
+        Serial.print("\t\n");
     }
 }
